8ada238cc31149cf92bce1ed9167a837427b895d
Alexandre <4lexcode@gmail.com>
2024-07-15 14:58:02 -0300

    Primeiro exemplo com LinkedHashMap

--- src/Collections/learnMap.java
+++ src/Collections/learnMap.java
@@ -1,5 +1,8 @@
 package Collections;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,8 +12,33 @@ public class learnMap {
         Scanner sc = new Scanner(System.in);
 
         Map<String, Integer> votes = new LinkedHashMap<>();
+        System.out.println("Enter file pull path:");
+        String path = sc.nextLine();
 
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+            while (line != null){
+                String[] fields = line.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
+
+                if (votes.containsKey(name)){
+                    int votesSoFar = votes.get(name);
+                    votes.put(name, count + votesSoFar);
+                }else{
+                    votes.put(name, count);
+                }
+                line = br.readLine();
+            }
+
+            for (String key : votes.keySet()){
+                System.out.println(key + ": " + votes.get(key));
+            }
+        }catch(IOException e){
+            System.out.println("Error" + e.getMessage());
+        }
 
         sc.close();
+        //colar: C:\Users\alexv\in.txt
     }
 }
