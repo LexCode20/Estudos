3f5a3b118fb30ffb51083a761639be0a4f731327
Alexandre <4lexcode@gmail.com>
2024-07-25 11:29:57 -0300

    Lendo um arquivo

--- src/HandlingFiles/ReadAFile.java
+++ src/HandlingFiles/ReadAFile.java
@@ -1,2 +1,22 @@
-package HandlingFiles;public class ReadAFile {
+package HandlingFiles;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadAFile {
+    public static void main(String[] args){
+        try {
+            File myFile = new File("C:\\Users\\alexv\\IdeaProjects\\Estudos\\src\\HandlingFiles\\familia.txt");
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
 }
