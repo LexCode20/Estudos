bfa0f25c58f0aa036b6a5c0872a6c9f6fedcde58
Alexandre <4lexcode@gmail.com>
2024-07-25 11:34:24 -0300

    Criando um arquivo

--- src/HandlingFiles/CreateFile.java
+++ src/HandlingFiles/CreateFile.java
@@ -1,2 +1,20 @@
-package HandlingFiles;public class CreateFile {
+package HandlingFiles;
+
+import java.io.IOException;
+import java.io.File;
+
+public class CreateFile {
+    public static void main(String[] args){
+        try {
+            File myFile = new File("C:\\Users\\alexv\\IdeaProjects\\Estudos\\src\\HandlingFiles\\familia.txt");
+            if (myFile.createNewFile()){
+                System.out.println("File created: " + myFile.getName());
+            }else{
+                System.out.println("File already exists.");
+            }
+        }catch(IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
