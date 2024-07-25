8eee1ea15bd7869156bd3a494ced67b9ca12b60c
Alexandre <4lexcode@gmail.com>
2024-07-25 11:31:58 -0300

    Escrevendo em um arquivo txt

--- src/HandlingFiles/WriteToAFile.java
+++ src/HandlingFiles/WriteToAFile.java
@@ -1,2 +1,19 @@
-package HandlingFiles;public class WriteToAFile {
+package HandlingFiles;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToAFile {
+    public static void main(String[] args){
+        try {
+            FileWriter myWriter = new FileWriter("C:\\Users\\alexv\\IdeaProjects\\Estudos\\src\\HandlingFiles\\familia.txt");
+            myWriter.write("My name is Alexandre but can call me Alex\nI'm twenty years old\nMy family is also Laís\nI love her so much");
+            myWriter.close();
+            System.out.println("Sucessfully wrote to the file.");
+        }catch(IOException e){
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
 }
